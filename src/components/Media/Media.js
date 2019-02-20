05467b65dcd33fa618819dec704d8d2fb1d96478
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-02-19 18:31:49 -0600

    remove carousel legends

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -8,28 +8,21 @@ const Media = ({ mediaStuff }) => {
     <Carousel autopaly>
       <div>
         <img src={require("../../static/1.jpg")} alt="" />
-        <p className="legend">Legend 1</p>
       </div>
       <div>
         <img src={require("../../static/2.jpg")} alt="" />
-        <p className="legend">Legend 2</p>
       </div>
       <div>
         <img src={require("../../static/3.jpg")} alt="" />
-        <p className="legend">Legend 3</p>
       </div>
       <div>
         <img src={require("../../static/4.jpg")} alt="" />
-        <p className="legend">Legend 4</p>
       </div>
       <div>
         <img src={require("../../static/5.jpg")} alt="" />
-        <p className="legend">Legend 5</p>
       </div>
       <div>
         <img src={require("../../static/gettyimages-887636042.jpg")} alt="" />
-
-        <p className="legend">Legend 6</p>
       </div>
     </Carousel>
   );
