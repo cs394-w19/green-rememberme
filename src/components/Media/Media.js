0718663926c3f7b0622056e206419dfe50259070
Terry Tan <tmy1995hflc@gmail.com>
2019-02-19 23:42:10 -0600

    add video module

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -2,18 +2,14 @@ import React, { Component } from "react";
 import "./Media.css";
 import ReactDOM from "react-dom";
 import { Carousel } from "react-responsive-carousel";
+import ReactPlayer from 'react-player';
 
 const Media = ({ mediaStuff }) => {
   return (
     <div className='maxWidth'>
-      <Carousel autopaly>
+      <Carousel autoplay>
         <div>
-          <video
-            width="100%"
-            controls
-            src={require("../../static/video.mp4")}
-            type="video/mp4"
-          />
+          <ReactPlayer width="100%" controls pip url='https://www.youtube.com/watch?v=1StF6gHT4m8' playing />
         </div>
         <div>
           <img src={require("../../static/1.jpg")} alt="" />
