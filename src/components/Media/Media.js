806bda42637c9f1a57cab8c689af359bd090823d
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-02-19 18:15:01 -0600

    add carousel slider

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Media.css";
+import ReactDOM from "react-dom";
+import { Carousel } from "react-responsive-carousel";
 
-const Media = () => {
+const Media = ({ mediaStuff }) => {
   return (
-    <div className="Media">
-      <h3>Media</h3>
-    </div>
+    <Carousel autopaly>
+      <div>
+        <img src={require("../../static/1.jpg")} alt="" />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img src={require("../../static/2.jpg")} alt="" />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img src={require("../../static/3.jpg")} alt="" />
+        <p className="legend">Legend 3</p>
+      </div>
+      <div>
+        <img src={require("../../static/4.jpg")} alt="" />
+        <p className="legend">Legend 4</p>
+      </div>
+      <div>
+        <img src={require("../../static/5.jpg")} alt="" />
+        <p className="legend">Legend 5</p>
+      </div>
+      <div>
+        <img src={require("../../static/gettyimages-887636042.jpg")} alt="" />
+
+        <p className="legend">Legend 6</p>
+      </div>
+    </Carousel>
   );
 };
-
 export default Media;
