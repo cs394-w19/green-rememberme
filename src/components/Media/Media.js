7b1c4cd5a505d6e23380bc02cb6a6876e14bdd84
Terry Tan <tmy1995hflc@gmail.com>
2019-02-26 13:05:58 -0600

    update carousel

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,31 +1,42 @@
 import React from "react";
 import "./Media.css";
-import { Carousel } from "react-responsive-carousel";
+import Coverflow from "react-coverflow";
 import ReactPlayer from "react-player";
 
 const Media = ({ mediaStuff }) => {
+  var style = {
+    margin: "0 auto",
+    marginRight: "30px"
+  }
   return (
     <div className="maxWidth">
-      <Carousel>
-        {/* <div>
-          <ReactPlayer
-            width="100%"
-            controls
-            pip
-            url="https://www.youtube.com/watch?v=1StF6gHT4m8"
-            playing
-          />
-        </div> */}
-        <div>
-          <img src={require("../../static/1.jpg")} alt="" />
+      <Coverflow
+        width={900}
+        height={170}
+        displayQuantityOfSide={2}
+        navigation={false}
+        enableHeading={false}
+        style={style}
+      >
+        <div
+          role="menuitem"
+          tabIndex="0"
+        >
         </div>
-        <div>
-          <img src={require("../../static/2.jpg")} alt="" />
-        </div>
-        <div>
-          <img src={require("../../static/3.jpg")} alt="" />
-        </div>
-      </Carousel>
+        <img src={require("../../static/1.jpg")} alt="" />
+
+        <img src={require("../../static/2.jpg")} alt="" />
+
+        <img src={require("../../static/3.jpg")} alt="" />
+
+        <img src={require("../../static/daughter.png")} alt="" />
+
+        <img src={require("../../static/grandma.png")} alt="" />
+
+        <img src={require("../../static/mom.png")} alt="" />
+
+      </Coverflow>
+      <div style={{height:"30px"}}></div>
     </div>
   );
 };
