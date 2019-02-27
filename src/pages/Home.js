3adfacee80cf94be61bdb002ac0e57e40aa119dc
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2019-02-26 21:18:28 -0600

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -53,7 +53,7 @@ class Home extends Component {
 
   render() {
 
-    const recipes = [['Osso Bucco',1],['Pumpkin Bread',2],["Grandma's Pancakes",3], ['Cheezits',4],["Michael's Mac and Cheeze",5],["Terry's Terry-iaki",6],["Ryan's Cereal",7],["Cristobal's Rice",8],["Vanessa's Food",9]]
+    const recipes = [['Osso Bucco',1],['Pumpkin Bread',2],["Grandma's Pancakes",3], ['Cheezits',4],["Michael's Mac and Cheeze",5],["Terry's Terry-iaki",6],["Ryan's Cereal",7],["Cristobal's Rice",8],["Vanessa's Ramen",9]]
     const recipeList = recipes.map((recipe, i) => {
       let path = '/recipes/' + recipe[1]
       let a = recipe[0].toLowerCase()
