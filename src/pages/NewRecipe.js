4549d09adb9ee719ddb3a29d3b3f55c53f9c1eec
Michael Guo <michaelgguo@gmail.com>
2019-02-27 11:09:20 -0600

    removed email validation

--- src/pages/NewRecipe.js
+++ src/pages/NewRecipe.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react"
-import { withFirebase } from "../components/Firebase/"
-import "./Login.css"
-import "../App.css"
+import React, { Component } from "react";
+import { withFirebase } from "../components/Firebase/";
+import "./Login.css";
+import "../App.css";
 import Menu from "../components/Menu/Menu";
-import { Link, Redirect } from 'react-router-dom'
+import { Link, Redirect } from "react-router-dom";
 
 class NewRecipe extends Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      email:this.props.location.email,
-      loggedIn:false
-    }
+      email: this.props.location.email,
+      loggedIn: false
+    };
   }
 
   toggleMenu() {
@@ -39,53 +38,48 @@ class NewRecipe extends Component {
     }
   }
 
-  renderErrorEmail(){
-    if (this.state.error){
-      return(
-        <div className='errorText'>
-          {this.state.errorMessage}
-        </div>
-      )
+  renderErrorEmail() {
+    if (this.state.error) {
+      return <div className="errorText">{this.state.errorMessage}</div>;
     }
   }
 
-  handleSubmitEmail(e){
-    if (!this.validateEmail(this.state.email)){
+  handleSubmitEmail(e) {
+    if (!this.validateEmail(this.state.email)) {
       this.setState({
-        error:true,
-        errorMessage:'Invalid email...'
-      })
-    }
-    else{
-      console.log('here')
-      return(
-        this.setState({
-          loggedIn:true
-        })
-      )
+        error: true,
+        errorMessage: "Invalid email..."
+      });
+    } else {
+      console.log("here");
+      return this.setState({
+        loggedIn: true
+      });
     }
   }
 
-  validateEmail(email){
+  validateEmail(email) {
     /*eslint-disable */
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     /*eslint-enable */
     return re.test(email);
   }
 
-  handleInputEmail(e){
-    this.setState({email:e.target.value,error:false})
+  handleInputEmail(e) {
+    this.setState({ email: e.target.value, error: false });
   }
 
   render() {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0);
     if (this.state.loggedIn === true) {
-      return <Redirect
-                to={{
-                  pathname: "/home",
-                  email: this.state.email
-                }}
-              />
+      return (
+        <Redirect
+          to={{
+            pathname: "/home",
+            email: this.state.email
+          }}
+        />
+      );
     }
 
     return (
@@ -95,13 +89,15 @@ class NewRecipe extends Component {
         <div className="appLogo">
           <img className="mainLogo" src="/logo.png" alt="logo" />
         </div>
-        <div className='header'>New Recipe</div>
-        <br/><br/>
-        <div style={{width:'100%',textAlign:'center'}}> This feature coming soon!</div>
-        <Link to={{pathname:'/home', email:this.state.email}}>
-          <button className='buttonPrimary'>
-            back
-          </button>
+        <div className="header">New Recipe</div>
+        <br />
+        <br />
+        <div style={{ width: "100%", textAlign: "center" }}>
+          {" "}
+          This feature coming soon!
+        </div>
+        <Link to={{ pathname: "/home", email: this.state.email }}>
+          <button className="buttonPrimary">back</button>
         </Link>
         {this.renderMenu()}
       </div>
