d6774e85bcc92bf4442031cc6fb89a636f9ec9dd
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2019-03-05 22:44:12 -0600

    family emails page

--- src/pages/NewFamily.js
+++ src/pages/NewFamily.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withFirebase } from "../components/Firebase/";
-import "./Login.css";
+import "./NewRecipe.css";
 import "../App.css";
 import Menu from "../components/Menu/Menu";
 import { Link, Redirect } from "react-router-dom";
@@ -9,7 +9,9 @@ class NewFamily extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      familyEmails: []
+      email: this.props.location.email,
+      familyID: this.props.location.familyID,
+      instructions:[''],
     };
   }
 
@@ -17,62 +19,107 @@ class NewFamily extends Component {
     console.log(this.state)
   }
 
-
-
-//IN PROGRESS>>>>>>>>
-  async handleSubmitEmail(e) {
-    if (!this.validateEmail(this.state.email)) {
+  toggleMenu() {
+    console.log("toggling menu");
+    if (this.state.menu) {
       this.setState({
-        error: true,
-        errorMessage: "Invalid email..."
+        menu: false
       });
     } else {
-      console.log("received email");
-      // const familyID = await this.props.firebase.findFamily();
-      // window.setTimeout(()=>{
-      //   console.log(familyID)
-      //   if (familyID == null || familyID == -1){
-      //     console.log('handled the error')
-      //   } else {
-      //     this.setState({loggedIn: true});
-      //   }}, 2000)
-      this.setState({loggedIn: true});
-
+      this.setState({
+        menu: true
+      });
     }
+  }
+
+  renderMenu() {
+    if (this.state.menu) {
+      return (
+        <div>
+          <div className="menuWrapper" onClick={() => this.toggleMenu()} />
+            <Menu
+              toggle={() => this.toggleMenu.bind(this)}
+              email={this.state.email}
+              familyID={this.state.familyID}
+            />
+        </div>
+      );
+    }
+  }
+
+
+  addEmail(){
+    this.setState((prevState) => ({
+      instructions:[...prevState.instructions, '']
+    }))
+  }
+
+  renderEmail(){
+    const ins = this.state.instructions.map((val, i)=>{
+      let varname = "ins" + i
+      return(
+        <div className='inputContainer' key={i}>
+          <input className='inputInstruction' onChange={(e)=>this.setState({[varname]:e.target.value})}/>
+        </div>
+      )
+    })
+    return ins
+  }
+
+  createRecipeObject(){
+    let recipe={
+      instructions:[]
+    }
+    var i;
+    for (i=0; i < this.state.instructions.length; i++){
+      let insname = 'ins' + i
+      recipe.instructions.push(this.state[insname])
+    }
+
+    console.log(recipe)
+    this.writeRecipe(recipe)
+  }
+
+  writeRecipe(recipe){
+    console.log('there she goes')
+    let val = this.props.firebase.writeRecipe(recipe)
+    window.setTimeout(()=>{console.log(val)},2000)
   }
 
 
 
   render() {
-    if (this.state.loggedIn === true) {
-      window.scrollTo(0, 0);
-      return (
-        <Redirect
-          to={{
-            pathname: "/home",
-            email: this.state.email,
-            familyID: this.state.familyID
-          }}
-        />
-      );
-    }
-
     return (
       <div className="App">
-
-
-      <div className="section">
-        <div className="sectionHeader">Add Family Emails</div>
-        <input className='inputTitle' onChange={(e)=>this.setState({title:e.target.value})}/>
-      </div>
-
-
-          <button
-            className="buttonPrimary"
-            onClick={() => this.handleSubmitEmail()}>
-            LOGIN
-          </button>
+        {/* We will eventually want to move all this logic into a separate component
+          so we can access multiple recipes  */}
+        <div className="appLogo">
+          <Link to={{ pathname: "/home", email: this.state.email, familyID: this.state.familyID }}>
+            <img className="backImg" src="/back.png" alt="back" />
+          </Link>
+          <img className="logoImg" src="/logo.png" alt="logo" />
+          <img
+            className="menuImg"
+            src="/menu.png"
+            alt="menu"
+            onClick={() => this.toggleMenu()}
+          />
         </div>
+
+
+            <div className="section">
+            <div className="sectionHeader">Add Family Emails</div>
+            {this.renderEmail()}
+            <button className="addIngredient" onClick={()=>this.addEmail()}>
+              <img src="/plus.png" className="addIngredientImg" alt=""/>
+              add instruction
+            </button>
+            </div>
+
+
+        <button className="buttonPrimary" onClick={()=>this.createRecipeObject()}>Done</button>
+        {this.renderMenu()}
+
       </div>
     );
   }
