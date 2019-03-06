345e896c9d65eb43e67bd654d8b5365c59a495f5
Terry Tan <tmy1995hflc@gmail.com>
2019-03-06 10:43:48 -0600

    add images

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -210,6 +210,10 @@ class Media extends Component {
             <img key={url} src={url} alt="" />
           ))}
 
+          <img src="https://firebasestorage.googleapis.com/v0/b/remember-me-green.appspot.com/o/images%2F2.jpg?alt=media&token=f168afbe-c85c-49b2-bc33-db4e37485bb0"/>
+
+          <img src="https://firebasestorage.googleapis.com/v0/b/remember-me-green.appspot.com/o/images%2F3.jpg?alt=media&token=04c5de5d-14e2-4d8a-9a75-bebd72408d4b"/>
+
         </Carousel>
         <br />
 
