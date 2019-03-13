6905841bae72817e47a11e1496ec56155fe8b175
Michael Guo <michaelgguo@gmail.com>
2019-03-12 21:21:56 -0500

    added feature to separate by family ID (recipes). Not turned on because need to ask client for advice

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,12 +16,20 @@ class Home extends Component {
       menu: false,
       filter: "",
       allRecipes: [],
-      buttonsOn: 'buttonContainerHidden'
+      buttonsOn: "buttonContainerHidden"
     };
   }
 
   async componentDidMount() {
     const recipes = await this.props.firebase.readRecipes();
+
+    // Working feature which separates recipes by family ID.
+    // TODO: ask client if they want to enable this feature or not.
+
+    // const recipesID = recipes.filter(
+    //   recipe => recipe.data.recipe.family === this.state.familyID
+    // );
+    // this.setState({ allRecipes: recipesID });
 
     this.setState({ allRecipes: recipes });
   }
@@ -58,33 +66,44 @@ class Home extends Component {
     this.setState({ filter: e.target.value });
   }
 
-  buttonClass(button){
-    return this.state.selectedFilter === button ? 'button buttonOn' : 'button buttonOff'
+  buttonClass(button) {
+    return this.state.selectedFilter === button
+      ? "button buttonOn"
+      : "button buttonOff";
   }
 
-  toggleButton(button){
-    if (button === this.state.selectedFilter){
-      this.setState({selectedFilter:null})
-      return
+  toggleButton(button) {
+    if (button === this.state.selectedFilter) {
+      this.setState({ selectedFilter: null });
+      return;
     }
-    this.setState({selectedFilter:button})
+    this.setState({ selectedFilter: button });
   }
 
-  renderToggleText(){
-    if (this.state.buttonsOn === 'buttonContainer'){
-      return(<div className='linktext' onClick={()=>this.toggleButtons()}>hide category filters...</div>)
-    }
-    else{
-      return(<div className='linktext' onClick={()=>this.toggleButtons()}>show category filters...</div>)
+  renderToggleText() {
+    if (this.state.buttonsOn === "buttonContainer") {
+      return (
+        <div className="linktext" onClick={() => this.toggleButtons()}>
+          hide category filters...
+        </div>
+      );
+    } else {
+      return (
+        <div className="linktext" onClick={() => this.toggleButtons()}>
+          show category filters...
+        </div>
+      );
     }
   }
 
-  toggleButtons(){
-    if (this.state.buttonsOn === 'buttonContainer'){
-      this.setState({buttonsOn:'buttonContainerHidden', selectedFilter:null})
-    }
-    else{
-      this.setState({buttonsOn:'buttonContainer'})
+  toggleButtons() {
+    if (this.state.buttonsOn === "buttonContainer") {
+      this.setState({
+        buttonsOn: "buttonContainerHidden",
+        selectedFilter: null
+      });
+    } else {
+      this.setState({ buttonsOn: "buttonContainer" });
     }
   }
 
@@ -95,26 +114,30 @@ class Home extends Component {
       let path = "/recipes/" + object["id"];
       let a = object["data"]["recipe"]["title"].toLowerCase();
       let b = this.state.filter.toLowerCase();
-      let imgUrl = object["data"]["recipe"]["imageArray"][0]
-      if (imgUrl == null){imgUrl = '/default-image.jpg'}
+      let imgUrl = object["data"]["recipe"]["imageArray"][0];
+      if (imgUrl == null) {
+        imgUrl = "/default-image.jpg";
+      }
       if (a.includes(b)) {
-        if (this.state.selectedFilter === object["data"]["recipe"]["category"] || !this.state.selectedFilter){
-        return (
-          <Link
-            key={i}
-            to={{
-              pathname: path,
-              email: this.state.email,
-              familyID: this.state.familyID
-            }}
-          >
-            <div key={i} className="nameBody">
-              <img className='recipeImg' src={imgUrl} />
-              {" "}
-              {object["data"]["recipe"]["title"]}{" "}
-            </div>
-          </Link>
-        );
+        if (
+          this.state.selectedFilter === object["data"]["recipe"]["category"] ||
+          !this.state.selectedFilter
+        ) {
+          return (
+            <Link
+              key={i}
+              to={{
+                pathname: path,
+                email: this.state.email,
+                familyID: this.state.familyID
+              }}
+            >
+              <div key={i} className="nameBody">
+                <img className="recipeImg" src={imgUrl} />{" "}
+                {object["data"]["recipe"]["title"]}{" "}
+              </div>
+            </Link>
+          );
         }
       }
     });
@@ -151,27 +174,38 @@ class Home extends Component {
         </div>
 
         <div className={this.state.buttonsOn}>
-          <button className={this.buttonClass('Breakfast')} onClick={()=>this.toggleButton('Breakfast')}>
-            <div className='buttonImgText'>B</div>
-            <div className='buttonDescription'>Breakfast</div>
+          <button
+            className={this.buttonClass("Breakfast")}
+            onClick={() => this.toggleButton("Breakfast")}
+          >
+            <div className="buttonImgText">B</div>
+            <div className="buttonDescription">Breakfast</div>
           </button>
 
-          <button className={this.buttonClass('Lunch')} onClick={()=>this.toggleButton('Lunch')}>
-            <div className='buttonImgText'>L</div>
-            <div className='buttonDescription'>Lunch</div>
+          <button
+            className={this.buttonClass("Lunch")}
+            onClick={() => this.toggleButton("Lunch")}
+          >
+            <div className="buttonImgText">L</div>
+            <div className="buttonDescription">Lunch</div>
           </button>
 
-          <button className={this.buttonClass('Dinner')} onClick={()=>this.toggleButton('Dinner')}>
-            <div className='buttonImgText'>D</div>
-            <div className='buttonDescription'>Dinner</div>
+          <button
+            className={this.buttonClass("Dinner")}
+            onClick={() => this.toggleButton("Dinner")}
+          >
+            <div className="buttonImgText">D</div>
+            <div className="buttonDescription">Dinner</div>
           </button>
 
-          <button className={this.buttonClass('Dessert')} onClick={()=>this.toggleButton('Dessert')}>
-            <div className='buttonImgText'>D</div>
-            <div className='buttonDescription'>Dessert</div>
+          <button
+            className={this.buttonClass("Dessert")}
+            onClick={() => this.toggleButton("Dessert")}
+          >
+            <div className="buttonImgText">D</div>
+            <div className="buttonDescription">Dessert</div>
           </button>
         </div>
-
 
         <div className="inputContainer">
           <input
